06bad9fd6d645809e9851d32b32beee8268be9d5
Camilo <cbarraza11@gmail.com>
2014-03-16 01:45:28 -0500

    con cambios para que funcione esa tavuel

--- config/application.rb
+++ config/application.rb
@@ -58,7 +58,7 @@ module Untitledapp
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     #config.active_record.whitelist_attributes = true
-cmd
+
     # Enable the asset pipeline
     config.assets.enabled = true
 
